50c36d512ff05b10b43fa40ec00d5f0b3496176a
aymane <elmainiaymane03@gmail.com>
2023-06-25 03:19:47 +0100

    finish

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
   listContainer.addEventListener("click", checkTask);
 
+  // Load tasks from local storage on page load
+  loadTasksFromStorage();
+
   function addTask() {
     if (inputBox.value === '') {
       alert("You must write something!");
@@ -16,6 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
       li.classList.add("task");
       listContainer.appendChild(li);
       inputBox.value = '';
+
+      // Save tasks to local storage
+      saveTasksToStorage();
     }
   }
 
@@ -23,6 +29,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const clickedElement = event.target;
     if (clickedElement.tagName === 'LI') {
       clickedElement.classList.toggle("checked");
+
+      // Save tasks to local storage
+      saveTasksToStorage();
+    }
+  }
+
+  function saveTasksToStorage() {
+    const tasks = Array.from(listContainer.getElementsByTagName("li")).map(li => li.innerHTML);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  function loadTasksFromStorage() {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      const tasks = JSON.parse(storedTasks);
+      tasks.forEach(task => {
+        let li = document.createElement("li");
+        li.innerHTML = task;
+        li.classList.add("task");
+        listContainer.appendChild(li);
+      });
     }
   }
 });
